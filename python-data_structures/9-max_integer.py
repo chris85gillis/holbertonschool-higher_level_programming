eec95f9a6fd79279a2a3ecd8d146b9f2ba72b254
chris85gillis <chris85gillis@gmail.com>
2023-09-11 13:21:14 -0700

    trying to write code in a different way to get better resutls with the checker

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,12 +2,13 @@
 # 9-max_integer.py
 
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return (None)
+    if not my_list
+        return None
 
-    big = my_list[0]:
-    for i in range(len(my_list)):
-        if my_list[i] > big:
-            big = my_list[i]
+    max_value = my_list[0]
 
-    return (big)
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+
+    return max_value
