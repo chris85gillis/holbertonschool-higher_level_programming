f28c09f9496e8cd4c4ce79c18b7669ddd5be55f3
chris85gillis <chris85gillis@gmail.com>
2023-09-11 12:17:14 -0700

    fix 5-no file to work prop with main

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,10 +2,5 @@
 # 5-no_c.py
 
 def no_c(my_string):
-    new_string = ""
-
-    for char in my_string
-        if char != 'c' and char != 'C':
-            new_string += char
-
-    return new_string
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
