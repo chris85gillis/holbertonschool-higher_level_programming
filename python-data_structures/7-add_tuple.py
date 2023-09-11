7dc3e9302a2dbfb977b3c26aeee0cf6689582293
chris85gillis <chris85gillis@gmail.com>
2023-09-11 12:41:09 -0700

    added fixes to get better output on file 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a(), tuple_b=()):
-    a = tuple_a + (0, 0)
-    b = tuple_b + (0, 0)
-
-    result_tuple = (a[0] + b[0], a[1] + b[1])
-
-    return result_tuple
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
