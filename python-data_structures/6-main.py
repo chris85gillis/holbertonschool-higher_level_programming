487b613e8c22a135aa87222f1e169af6917de7d4
chris85gillis <chris85gillis@gmail.com>
2023-09-11 12:25:50 -0700

    added main file for 6

---
+++ python-data_structures/6-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 7, 9],
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
