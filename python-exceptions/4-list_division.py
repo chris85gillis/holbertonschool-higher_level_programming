e71b9d2e3d648239ea0a3022e90f1f369d1d6017
chris85gillis <chris85gillis@gmail.com>
2023-09-12 15:50:40 -0700

    seperated errors to get more check

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,7 +8,10 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             div = 0
-        except (TypeError, IndexError):
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except IndexError:
             print("out of range")
             div = 0
         finally:
