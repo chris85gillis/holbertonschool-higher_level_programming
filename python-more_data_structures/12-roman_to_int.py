3395558d71c2fd7efd148320ec1d3e2d6eb904b7
chris85gillis <chris85gillis@gmail.com>
2023-09-12 11:32:02 -0700

    removed file 12 and started over

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,39 +1,25 @@
 #!/usr/bin/python3
-def to_subtract(list_num):
-    to_sub = 0
-    max_list = max(list_num)
 
-    for n in list_num:
-        if max_list > n:
-            to_sub += n
-
-    return (max_list - to_sub)
-
-
-def roman_to_int(roman_string, str):
-    if not roman_string:
+def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
         return 0
 
-    if not isinstance(roman_string, str):
-        return 0
+    roman_numerals = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+    }
 
-    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(rom_n.keys())
+    result = 0
+    prev_value = 0
 
-    num = 0
-    last_rom = 0
-    list_num = [0]
+    for char in reversed(roman_string):
+        current_value = roman_numerals.get(char, 0)
 
-    for ch in roman_string:
-        for r_num in list_keys:
-            if r_num == ch:
-                if rom_nget(ch) <= last_rom:
-                    num += to _subtract(list_num)
-                    list_num = [rom_n.get(ch)]
-                else:
-                    list_num.append(rom_n.get(ch))
+        if current_value >= prev_value:
+            result += current_value
+        else:
+            result -= current_value
 
-                last_rom = rom_n.get(ch)
-    num += to_subtract(list_num)
+        prev_value = current_value
 
-    return (num)
+return result
