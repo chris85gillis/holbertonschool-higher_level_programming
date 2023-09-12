3405140d3ee6fe48bb1b0b48acb4739059bd802a
chris85gillis <chris85gillis@gmail.com>
2023-09-12 09:41:46 -0700

    trying out something different for file 5

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 def number_keys(a_dictonary):
-    num = 0
-    list_keys = list(a_dictionary.keys())
-
-    for i in list_keys:
-        num += 1
-
-    return (num)
+    count = 0
+    for key in a_dictionary):
+        count += 1
+    return count
