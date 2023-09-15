08fe14973d0f07f4645bcc9f218b8e1186c90a9c
chris85gillis <chris85gillis@gmail.com>
2023-09-15 12:32:31 -0700

    trying a different way of writting the code for 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,22 +2,29 @@
 """Demonstrates the usage of the add_integer function."""
 
 
-add_integer= __import__('0-add_integer').add_integer
-"""Addes two integers and returns their sum.
-    Args:
-        a (int): The first integer to be added.
-        b (int): The second integer to be added.
-"""
+def add_integer(a, b=98):
+    """
+    Adds two integers.
 
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
+    Args:
+        a (int or float): The first integer or float.
+        b (int or float, optional): The second integer or float. Default is 98.
+
+    Returns:
+        int: The sum of a and b, casted to an integer.
+
+    Raises:
+        TypeError: If either 'a' or 'b' is not an integer or float.
+
+    Example:
+        result = add_integer(3, 5)  # result will be 8
+        result = add_integer(-1, 2)  # result will be 1
+
+    """
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
