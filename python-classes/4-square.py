57428648f04aba57b4b2c5700b1f8f4496afdf37
chris85gillis <chris85gillis@gmail.com>
2023-09-15 10:58:18 -0700

    removed blank spaces from file 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Represent a square."""
-    
+
     def __init__(self, size=0):
         """Initialize a new square.
         Args:
@@ -28,4 +28,3 @@ class Square:
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
-        
