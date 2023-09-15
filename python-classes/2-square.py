c32a018509a7340e69fa30b46fcbef17a0f41df0
chris85gillis <chris85gillis@gmail.com>
2023-09-14 19:57:08 -0700

    added style corrections to both 1 and 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,11 +4,11 @@
 
 class Square:
     """Represent a square."""
-    
+
     def __init__(self, size=0):
         """Initialize a new Square.
         Args:
-        	size (int): The size of the new square.
+            size (int): The size of the new square.
          """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
