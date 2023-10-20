ca8452cd120fcf9501d8585733b941bdd983526d
chris85gillis <chris85gillis@gmail.com>
2023-10-20 10:20:15 -0700

    chasing checks

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,13 @@
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+# Create 'user_0d_1' user and grant privileges
+echo "CREATE USER 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+
+# Create 'user_0d_2' user and grant privileges
+echo "CREATE USER 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+
+# List privileges for 'user_0d_1'
+cat 0-privileges.sql | mysql -hlocalhost -uuser_0d_1 -p
+
+# List privileges for 'user_0d_2'
+cat 0-privileges.sql | mysql -hlocalhost -uuser_0d_2 -p
