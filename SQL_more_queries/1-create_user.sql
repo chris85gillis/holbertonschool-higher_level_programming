07590a2f227607f96340319b876200fac8fec671
chris85gillis <chris85gillis@gmail.com>
2023-10-20 10:29:44 -0700

    worked on formatting for file 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- Create 'user_0d_1' user with all privileges and set the password if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
 -- Grant all privileges to 'user_0d_1'
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
