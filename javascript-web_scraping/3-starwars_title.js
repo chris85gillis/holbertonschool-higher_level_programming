5cab3b65b10069ee0d6da3186df6058333fcb3fd
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2023-11-30 20:34:49 +0000

    fixing code for task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
+
+let id = process.argv[2];
+let url = 'http://swapi.co/api/films/' + id;
 const request = require('request');
-const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
-  if (error == null) {
-    const json = JSON.parse(body);
-    console.log(json.title);
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    console.log(body['title']);
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
   }
 });
